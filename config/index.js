3075bfe3043fe185b63c1b705861edf55350ad20
‘lg’ <740553658@qq.com>
2024-03-25 17:33:28 +0800

    api更换

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {
       '*': {
         //target: "https://virtserver.swaggerhub.com/cybseer/webclient/0.1.0/",
-        target: "https://cybseer.vokeylee.com/api/",
+        // target: "https://cybseer.vokeylee.com/api/",
+        target: "http://10.128.20.20:4321/api/",
         changeOrigin: true,
       }
     },
